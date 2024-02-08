36a9661354e959a593fc1d97badb3cd6e2a69a8f
Sanieeme <shoneesani@gmail.com>
2024-02-08 04:27:39 -0800

    task 6

---
+++ models/engine/console.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+
+    prompt = "(hbnb)"
+    """custom prompty (hbnb"""
+
+    def do_quit(self, line):
+        """quit program"""
+        return (True)
+
+    def do_EOF(self, line):
+        """exit program"""
+        return True
+
+    def do_emptyline(self):
+        """shouldnt execute anything"""
+        pass
+        
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
