08361e1cae7f8203e814e7eee1e8820a5a5769da
Sanieeme <shoneesani@gmail.com>
2024-02-10 04:46:48 -0800

    checkout

--- console.py
+++ console.py
@@ -25,7 +25,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, ln):
         """creating new instances"""
-        if not ln :
+        if not ln:
             print("** class name missing **")
         elif line not in storage.classes():
             print("** class doesn't exist **")
@@ -35,7 +35,9 @@ class HBNBCommand(cmd.Cmd):
             print(new.id)
 
     def do_show(self, ln):
-        """Prints the string representation of an instance based on the class name and id"""
+        """Prints the string representation of an instance
+        based on the class name and id
+        """
         args = ln.split()
 
         if not args:
@@ -66,8 +68,11 @@ class HBNBCommand(cmd.Cmd):
                 print("** no instance found **")
             else:
                 del storage.all()[k]
+
     def do_all(self, ln):
-        """Prints all string representation of all instances based or not on the class name"""
+        """Prints all string representation of all instances
+        based or not on the class name
+        """
         if not ln:
             print("** class doesn't exist **")
         else:
@@ -75,6 +80,7 @@ class HBNBCommand(cmd.Cmd):
             for n in d:
                 if n.__class__.__name__ == ln:
                     print(n)
+
     def do_update(self, ln):
         args = ln.split()
         if not args:
@@ -84,11 +90,18 @@ class HBNBCommand(cmd.Cmd):
         elif len(args) < 2:
             print("** instance id missing **")
         else:
-            k = args[0] + "." +args[1]
+            k = args[0] + "." + args[1]
             if k not in storage.all():
                 print("** no instance found **")
-
-
+            else:
+                if len(args) < 3:
+                    print("** attribute name missing **")
+                elif len(args) < 4:
+                    print("** value missing **")
+                else:
+                    a = args[2]
+                    v = args[3]
+                    print(f"{k} {a} = {v}")
 
 
 if __name__ == '__main__':
