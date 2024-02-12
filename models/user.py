a4815969a146a96d901b9f05d3a474a0749b5499
Sanieeme <shoneesani@gmail.com>
2024-02-11 18:39:33 -0800

    s

--- models/user.py
+++ models/user.py
@@ -4,21 +4,8 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    num = 0
-
-    def __init__(self, email="", password="", first_name="", last_name=""):
-        """constructor"""
-        super().__init__(name=email, age=password)
-        self.email = email
-        self.password = password
-        self.first_name = first_name
-        self.last_name = last_name
-
-        User.num += 1
-
-    def display_user(self):
-        print(f"{self.email}, {self.password}, {self.first_name}, \
-                {self.last_name}")
-
-
-print(f"{User.num}")
+    """class for user"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
